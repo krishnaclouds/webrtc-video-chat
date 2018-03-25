4a954fabd2ffd97a2e98036aff3c82b8d3fcd957
krishnaclouds <prince.balakrishna@gmail.com>
2018-03-26 01:15:26 +0530

    Capturing a Picture

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 9000;
+const port = 3000;
 const path = require('path');
 
 const app = express();
--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,40 @@ Exploring MediaStream or getUserStream API
 
 //Video Tag element to Attach MediaStream Object
 var video = document.querySelector("video");
+var videoTag = document.getElementById("localVideo");
+var profilePicBtn = document.getElementById("profilePicBtn");
+var profilePicCanvas = document.getElementById("profilePicCanvas");
+var profilePicOutput = document.getElementById("profilePicOutput");
+
+var width = 320;
+var height = 320;
+var streaming = false;
+
+
+
+profilePicBtn.addEventListener('click', function(event){
+    console.log("Click Event Generated");
+    takeProfilePic();
+    event.preventDefault();
+}, false);
+
+function takeProfilePic() {
+    var context = profilePicCanvas.getContext('2d');
+    if (width && height) {
+        profilePicCanvas.width = width;
+        profilePicCanvas.height = height;
+
+        context.drawImage(videoTag, 0, 0, width, height);
+
+        var data = profilePicCanvas.toDataURL('image/png');
+        profilePicOutput.setAttribute('src', data);
+    } else {
+
+        context.drawImage(videoTag, 0, 0, profilePicCanvas.width, profilePicCanvas.height);
+        var data = profilePicCanvas.toDataURL('image/png');
+        profilePicOutput.setAttribute('src', data);
+    }
+}
 
 /*
 Navigator.getUserMedia -> Chrome or in general
@@ -15,20 +49,30 @@ navigator.getUserMedia = navigator.getUserMedia ||
     navigator.mozGetUserMedia ||
     navigator.webkitGetUserMedia;
 
+/*
+MediaStream Constraints - More at:
+https://developer.mozilla.org/en-US/docs/Web/API/Media_Streams_API/Constraints
+*/
 navigator.getUserMedia({
     video: {
-        maxWidth: 320,
-        maxHeight: 180
+        width:{
+            min:320,
+            max:320
+        },
+        height:{
+            min:320,
+            max:320
+        }
     },
     audio: false
-}, function onSuccess(stream) {
+}, function successCallback(stream) {
 
     //Successfully retrieved Media Stream from User; so attach it to the Video tag.
     console.log("Successfully retrieved user Stream using Navigator.getUserMedia");
     video.srcObject = stream;
     video.play();
 
-}, function onError(err) {
+}, function errorCallback(err) {
     // Certain Error in retrieving Media Stream
     console.log(err);
 });
